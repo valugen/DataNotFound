157cb405db5995d981e57f50c84ded24d8be8974
gonzaggit <gonzalo.guevara95@gmail.com>
2025-05-26 22:57:18 -0300

    Proyecto evidencia 2 SkyRoute

--- main.py
+++ main.py
@@ -16,17 +16,14 @@ Pasos a seguir:
 2. Hacer doble click y abrir el archivo o correr en la terminal con: "python SkyRoute.py".
 3. Seguir las instrucciones indicadas.
 
+Grupo: DataNotFound
+
 Integrantes:
 - Cabrera, Gonzalo (41501046)
 - Gesto, Valentina (41893604)
 - Guevara, Gonzalo (38802553)
 - Herrera, Bruno   (46592052)
 - Theaux, Jimena   (40928800)
-
-#Datos de los integrantes del grupo (nombre, apellido y DNI).
-#El menú de opciones.
-#Control de flujo de acciones a realizar según la opción seleccionada (sin modularización).
-#grupo: DataNotFound
 
 """
 #Variables
